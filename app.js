22625b9462700a98e79c958f9d930f767bd343c5
dilleepp <dileep.iitr@gmail.com>
2013-03-18 23:02:08 +0530

    updated app

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.use(express.basicAuth('panku', 'roorkee'));
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
